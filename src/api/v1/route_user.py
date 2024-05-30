a2deefa8a5ade869b3b232c3441315698469a961
alexmagalhaesdev <summer.magalhaes@gmail.com>
2024-05-29 21:12:59 -0300

    some crud operations

--- src/api/v1/route_user.py
+++ src/api/v1/route_user.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from schemas.user import UserCreate
+from schemas.user import UserCreate, ShowUser
 from db.session import get_db
 from db.repository.user import create_new_user
 
@@ -9,7 +9,7 @@ from db.repository.user import create_new_user
 router = APIRouter()
 
 
-@router.post("/")
+@router.post("/", response_model=ShowUser, status_code=status.HTTP_201_CREATED)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user, db=db)
     return user
---
+++ src/db/repository/blog.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+from schemas.blog import CreateBlog, UpdateBlog
+from db.models.blog import Blog
+
+
+def create_new_blog(blog: CreateBlog, db: Session, author_id: int = 1):
+    blog = Blog(title=blog.title, slug=blog.slug, author_id=author_id)
+
+    db.add(blog)
+    db.commit()
+
+    db.refresh(blog)
+
+    return blog
+
+
+def retrieve_blog(id: int, db: Session):
+    blog = db.query(Blog).filter(Blog.id == id).first()
+
+    return blog
+
+
+def list_blogs(db: Session):
+    blog = db.query(Blog).all()
+
+    return blog
+
+
+def update_blog_by_id(id: int, blog: UpdateBlog, db: Session, author_id: int = 1):
+    blog_in_db = db.query(Blog).filter(Blog.id == id).first()
+    if not blog_in_db:
+        return
+
+    blog_in_db.title = blog.title
+    blog_in_db.content = blog.content
+
+    db.add(blog_in_db)
+    db.commit()
+
+    return blog_in_db
